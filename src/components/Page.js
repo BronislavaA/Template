7bbe9bb54b0d865fbac9f0de3827fe0c28bc1c3b
Gabukas <gabukas@new-host-2.broadband>
2019-10-09 04:18:22 +0100

    react preparation for work

---
+++ src/components/Page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import logo from '../assets/images/logo.svg';
+import '../assets/sass/Page.scss';
+
+function Page() {
+  return (
+    <div className="page">
+    </div>
+  );
+}
+
+export default Page;
